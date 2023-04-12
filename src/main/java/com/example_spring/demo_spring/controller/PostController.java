a5c70b9738c96f5d9a82fd32f0c364819e70a8bb
franc.koukougnon <frznckkoukougnon@yahoo.fr>
2023-04-12 12:22:31 +0200

    methode Get

--- src/main/java/com/example_spring/demo_spring/controller/PostController.java
+++ src/main/java/com/example_spring/demo_spring/controller/PostController.java
@@ -1,4 +1,15 @@
 package com.example_spring.demo_spring.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class PostController {
+
+
+    @PostMapping("/firstpost")
+    public String FirstPost(){
+        return " Ceci est un exercice";
+    }
 }
